1c3dee7ec1d50e4513255031ddb1fcd096166465
vaibhav-singal <70965332+vaibhav-singal@users.noreply.github.com>
2020-11-06 18:11:22 +0530

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,42 @@
+class Ball {
+
+  
+    constructor(x,y,width,height) {
+
+      var options = {
+       
+        density: 1.0,
+        frictionAir: 0.005
+      }
+
+       
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+        World.add(world,this.body);
+        
+    }
+
+    display() {
+
+        
+            var pos = this.body.position;
+                var angle = this.body.angle;
+                push ();
+                translate(pos.x,pos.y);
+                rotate(angle);
+                ellipseMode(CENTER);
+                
+                ellipse(0,0,this.width,this.height);
+                pop ();
+          
+        
+         
+        
+          
+        
+    }
+}
+  
+
+
